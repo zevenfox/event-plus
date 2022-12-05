7cfe398c7c10f6d8c96e8e38650b004e3915af10
zevenfox <micky2smart1@gmail.com>
2022-12-06 02:23:50 +0700

    add base

--- eventsite/event/views.py
+++ eventsite/event/views.py
@@ -6,7 +6,7 @@ from django.utils.timezone import get_current_timezone
 
 # Create your views here.
 
-def home(request,year,month):
+def home(request,year=datetime.now().year,month=datetime.now().strftime('%B')):
     name = "Micky"
     month = month.title()
     #Covert Moth from name to number
@@ -16,4 +16,4 @@ def home(request,year,month):
     # get_current_timezone
     now = datetime.now(get_current_timezone())
     time = now.strftime("%H:%M %d %b %Y")
-    return render(request, 'home.html',{"name":name,"year":year, "month":month, "month_number":month_number, "cal":cal, "time":time})
+    return render(request, 'event/home.html',{"name":name,"year":year, "month":month, "month_number":month_number, "cal":cal, "time":time})
